172b61c0806df1ef1a36333059b2f51fd182bd7a
yc.andrewl <yc.andrewl@gmail.com>
2021-10-28 10:09:22 +0800

    Add second file

---
+++ monthly_get_R.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 15:11:18 2021
+
+@author: AndrewL
+"""
+
+import pandas as pd
+import requests
+from io import StringIO
+import time
+
+def monthly_report(year, month):
+    
+    # 假如是西元，轉成民國
+    if year > 1990:
+        year -= 1911
+    
+    url = 'https://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(year)+'_'+str(month)+'_0.html'
+    if year <= 98:
+        url = 'https://mops.twse.com.tw/nas/t21/sii/t21sc03_'+str(year)+'_'+str(month)+'.html'
+    
+    print(url)
+
+    # 偽瀏覽器
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    
+    # 下載該年月的網站，並用pandas轉換成 dataframe
+    #r = requests.get(url, headers=headers)
+    r = requests.get(url)
+    r.encoding = 'big5'
+
+    dfs = pd.read_html(StringIO(r.text), encoding='big-5')
+
+    df = pd.concat([df for df in dfs if df.shape[1] <= 11 and df.shape[1] > 5])
+    
+    if 'levels' in dir(df.columns):
+        df.columns = df.columns.get_level_values(1)
+    else:
+        df = df[list(range(0,10))]
+        column_index = df.index[(df[0] == '公司代號')][0]
+        df.columns = df.iloc[column_index]
+    
+    df['當月營收'] = pd.to_numeric(df['當月營收'], 'coerce')
+    df = df[~df['當月營收'].isnull()]
+    df = df[df['公司代號'] != '合計']
+    
+    # 偽停頓
+    time.sleep(5)
+
+    return df
+
+print (monthly_report(110,6))
